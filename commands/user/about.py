2af570e9b02fc1a6037921f12042693705a987a6
CarterPlenge <cplenge22@gmail.com>
2025-10-15 21:26:50 -0500

    formating

--- commands/user/about.py
+++ commands/user/about.py
@@ -7,13 +7,13 @@ def register(tree: app_commands.CommandTree, database, guild_id: int):
     async def about(interaction: Interaction):
         """Print info about the bot"""
         
-        message = """This bot is created by the collaborative effort of NWMSU Students.
+        message = """
+        This bot is created by the collaborative effort of NWMSU Students.
         
         --Check out the code and consider contributing at--
             https://github.com/GameMagma/NWMSU-Bot.git
         
         Its purpose is to aid in the management of NWMSU esports' discord servers
-        Type "/allCommands" to see a full list of commands.
         """
         
         await interaction.response.send_message(
